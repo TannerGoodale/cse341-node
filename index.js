610877b6f523359f6c40a9b1d18053ef2ed4ba07
Tanner Goodale <49888850+TannerGoodale@users.noreply.github.com>
2020-07-02 12:40:14 -0500

    fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const PORT = process.env.PORT || 5000
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
@@ -10,7 +11,7 @@ const app = express();
 const dbURI = "mongodb+srv://beta-user:Fishyfish1@node-tuts-ad5nm.mongodb.net/node-tuts?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
+  .then(result => app.listen(PORT))
   .catch(err => console.log(err));
 
 // register view engine
